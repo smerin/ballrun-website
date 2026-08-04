590b9f6a35b2d3db0ed1c0583cf8ddc042f36a1e
George Smerin <gsmerin@gmail.com>
2026-08-04 12:21:43 +0100

    Add survey links

---
+++ src/data/navigation.ts
@@ -0,0 +1,8 @@
+import { pages, external } from './urls';
+
+export const navLinks = [
+  { label: 'Bristol', href: pages.bristol },
+  { label: 'About', href: pages.about },
+  { label: 'Partners', href: pages.partners },
+  { label: 'Survey', href: external.survey },
+];
---
+++ src/data/urls.ts
@@ -0,0 +1,23 @@
+// =============================================================================
+// Site URLs and External Links
+// =============================================================================
+
+// Internal page URLs
+export const pages = {
+  home: '/',
+  bristol: '/bristol/',
+  about: '/about/',
+  partners: '/partners/',
+} as const;
+
+// External links
+export const external = {
+  survey: 'https://docs.google.com/forms/d/e/1FAIpQLScvO2Aear2kYqpN3ArvWtTZp6zFaSwaZldHT0SuDeBQlVH9sw/viewform',
+} as const;
+
+// Social media links (to be added)
+export const social = {
+  // instagram: '',
+  // facebook: '',
+  // twitter: '',
+} as const;
